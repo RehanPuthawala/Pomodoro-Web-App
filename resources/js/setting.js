3e08e1dcf0f160243c848d073177e5ad979669a4
Mo.Rehan <morehanp@gmail.com>
2020-09-11 13:56:20 +0600

    Setting Feature Implemented

--- resources/js/setting.js
+++ resources/js/setting.js
@@ -1,15 +1,56 @@
 const settingBtn = document.querySelector(".nav__btn");
 const settingBg = document.querySelector(".setting__background");
 const settingPopUp = document.querySelector(".setting__popUp");
+const settingClose = document.querySelector(".setting__close-icon");
+const settingOk = document.querySelector(".setting__ok");
 
-// *************** Setting Btn Event Listener ************ //
+const pomodoroTime = document.getElementById("pomodoro");
+const shortBreakTime = document.getElementById("sBreak");
+const longBreakTime = document.getElementById("lBreak");
 
+const soundVolumeInput = document.querySelector(".setting__range");
+const autoStartNextRound = document.getElementById("checkboxForNR");
+const darkMode = document.getElementById("checkboxForDM");
+
+// * User Setting Object * //
+
+const userSetting = {
+  time: {
+    pomodoro: 25,
+    sBreak: 5,
+    lBreak: 15,
+  },
+  autoStartNextRound: false,
+  soundVolume: 50,
+  darkMode: false,
+};
+
+// ? Setting Btn Event Listener //
 settingBtn.addEventListener("click", function () {
   settingBg.classList.add("setting__background--active");
+});
 
-  settingBg.addEventListener("click", function (e) {
-    if (e.target !== settingPopUp) {
-      settingBg.classList.remove("setting__background--active");
-    }
-  });
+// ? Setting Bg Add Event Listener
+settingBg.addEventListener("click", (e) => {
+  if (e.target.closest(".setting__popUp") !== settingPopUp) {
+    settingBg.classList.remove("setting__background--active");
+  }
+});
+
+// ? Close Button Add Event Listener
+settingClose.addEventListener("click", function () {
+  settingBg.classList.remove("setting__background--active");
+});
+
+//? Ok Button Event Listener
+settingOk.addEventListener("click", function () {
+  userSetting.time.pomodoro = parseInt(pomodoroTime.value);
+  userSetting.time.sBreak = parseInt(shortBreakTime.value);
+  userSetting.time.lBreak = parseInt(longBreakTime.value);
+
+  userSetting.soundVolume = parseInt(soundVolumeInput.value);
+  userSetting.autoStartNextRound = autoStartNextRound.checked;
+  userSetting.darkMode = darkMode.checked;
+
+  settingBg.classList.remove("setting__background--active");
 });
